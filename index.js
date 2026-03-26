33acb477e7f453331c95091dcf68f98637d6ed95
Natalie Morales <natalie@ephoria-arts.com>
2026-03-26 02:55:58 -0400

    adding ticket button

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ require('dotenv').config()
 
 const { Client, GatewayIntentBits, Partials } = require('discord.js')
 const { registerCoreEvents } = require('./events/registerCoreEvents')
-
+const { getTicketPanel } = require('./tickets/ticketpanel')
+ 
 const client = new Client({
   intents: [
     GatewayIntentBits.Guilds,
@@ -14,5 +15,12 @@ const client = new Client({
 })
 
 registerCoreEvents(client)
+
+client.once('ready', async () => {
+  console.log(`Bot is online as ${client.user.tag}`)
+
+  const channel = await client.channels.fetch('1486435994512392233')
+  await channel.send(getTicketPanel())
+})
 
 client.login(process.env.DISCORD_TOKEN)
